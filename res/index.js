003f1a9e15731da798e2458d0bbdaca958a49c69
Strudy <proteximportsexports@gmail.com>
2018-01-20 22:30:39 -0500

    Create index.js

---
+++ res/index.js
@@ -0,0 +1,23 @@
+
+writeidea = function () {
+   var idea = new Array();
+   idea[0] = "Make A Simple Website";
+    idea[1] = "Make A Online radio in html";
+    idea[2] = "Make A Mobile Browser";
+    idea[3] = "Make A Simple Website";
+    idea[4] = "Social Media Site";
+     idea[5] = "Make A discord/slack bot";
+    idea[6] = "Make your own website";
+    idea [7] = "Help someone code a website";
+     idea[8] = "Help in a open-source repo on github/or any software ( gitlab,bitbucket,etc";
+     idea[9] = "Make A Img site smillar to unsplash";
+     idea[10] = "Make a video editor"
+  
+   var rand = Math.floor(Math.random(1)*quote.length);
+   document.write(quote[rand]);
+ }
+
+// Inspired by https://github.com/vutondesign/bounce, This is not a copy, it used simple idea and simple knowledge, this is copied,
+/// Clamied By Mountio (Mountios.github.io/github.com/mountios/) ///
+
+ writeidea();
